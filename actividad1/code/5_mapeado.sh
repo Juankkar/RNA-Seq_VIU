e921a34f0fd62c6de60e5ae4c8f23e4af21d0e59
JuanCarlos <jcge97@gmail.com>
2023-02-16 18:23:23 +0000

    .

--- actividad1/code/5_mapeado.sh
+++ actividad1/code/5_mapeado.sh
@@ -8,8 +8,10 @@ then
    hisat2-build  ../data/raw/genoma.fa ../data/reference_genome/genome
 
    ## Realizamos el alineamiento con el genoma de referencia indexado
-   hisat2 -k2 -U ../data/processed/ejercicio2_trimmed/SRR13795616_trimmed.fq.gz -x ../data/reference_genome/genome --rna-strandness R -S ../results/mapeado/SRR13795616_hisat2.sam
+   hisat2 -k1 -U ../data/processed/ejercicio2_trimmed/SRR13795616_trimmed.fq.gz -x ../data/reference_genome/genome --rna-strandness R -S ../results/mapeado/SRR13795616_hisat2.sam
+   hisat2 -k2 -U ../data/processed/ejercicio2_trimmed/SRR13795616_trimmed.fq.gz -x ../data/reference_genome/genome --rna-strandness R -S ../results/mapeado/replica_hisat2.sam
 else
    echo "Los datos ya estaban mapeados"
 fi
+
 
