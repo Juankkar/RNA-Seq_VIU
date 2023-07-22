668c382eae251c7aaa56de27e24fefd0f1287e20
Juan Carlos García Estupiñán <104734299+Juankkar@users.noreply.github.com>
2023-07-22 17:02:39 +0100

    Delete actividad1 directory

--- actividad1/code/8_all_code.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-array=(1descarga_raw.sh 2_profundidad_len_fastqc.sh 4_trimmeado.sh 5_mapeado.sh 6_anotaciones_alienamientos.sh) 
-
-for i in ${array[*]} 
-do 
-    bash $i 
-done
